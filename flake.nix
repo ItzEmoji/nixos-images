2bb7a31206b967aa3e135290cf5d252615eafa13
ItzEmoji <no-reply@itzemoji.com>
2026-01-27 17:52:49 +0000

    trying to fix netboot

--- flake.nix
+++ flake.nix
@@ -1,54 +1,54 @@
-{
-  description = "NixOS Netboot (iPXE) + Installer ISO using Flakes";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, nixos-generators, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
   {
-    iso = nixos-generators.nixosGenerate {
-      inherit system;
-      format = "install-iso";
-      modules = [ ./configuration.nix ];
+    description = "NixOS Netboot (iPXE) + optional Installer ISO";
+
+    inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+      nixos-generators = {
+        url = "github:nix-community/nixos-generators";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
 
-    packages.${system}.netboot =
-      let
-        build = self.nixosConfigurations.netboot.config.system.build;
-      in
-      pkgs.linkFarm "netboot-images" [
-        {
-          name = "bzImage";
-          path = "${build.netbootKernel}/bzImage";
-        }
-        {
-          name = "initrd";
-          path = "${build.netbootRamdisk}/initrd";
-        }
-        {
-          name = "netboot.ipxe";
-          path = "${build.netbootIpxeScript}/netboot.ipxe";
-        }
-      ];
+    outputs = { self, nixpkgs, nixos-generators, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      iso = nixos-generators.nixosGenerate {
+        inherit system;
+        format = "install-iso";
+        modules = [ ./configuration.nix ];
+      };
 
-    nixosConfigurations.netboot = nixpkgs.lib.nixosSystem {
-      inherit system;
+      packages.${system}.netboot =
+        let
+          build = self.nixosConfigurations.netboot.config.system.build;
+          kernelFile =
+            self.nixosConfigurations.netboot.config.system.boot.loader.kernelFile;
+        in
+        pkgs.linkFarm "netboot-images" [
+          {
+            name = "bzImage";
+            path = "${build.kernel}/${kernelFile}";
+          }
+          {
+            name = "initrd";
+            path = "${build.netbootRamdisk}/initrd";
+          }
+          {
+            name = "netboot.ipxe";
+            path = "${build.netbootIpxeScript}/netboot.ipxe";
+          }
+        ];
 
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
-        ./configuration.nix
-      ];
+      nixosConfigurations.netboot = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
+          ./configuration.nix
+        ];
+      };
     };
-  };
-}
+  }
 
